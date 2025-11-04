160a38de7af5c07aced8317d803fc137c8103071
dbarai <dulalbarai01@gmail.com>
2025-11-04 08:54:42 +0530

    Configure Tailwind CSS with custom colors
    Add custom gray colors to Tailwind theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,18 @@
 
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          750: '#2D3748',
+          850: '#1F2937',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
